588cf69f1d5da90e4bedc9ba59fa201d5cdecf94
RamiresFilho <ramiresf.tech@gmail.com>
2026-02-05 00:59:26 -0300

    Service layer, component registration

--- src/main/java/com/rftech/webservice/repositories/UserRepository.java
+++ src/main/java/com/rftech/webservice/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.rftech.webservice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rftech.webservice.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 }
